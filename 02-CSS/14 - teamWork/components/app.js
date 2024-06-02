99a5df1fb30ebf5d423c4bd5f26e47c4bc21f023
Tanya1786 <tanya1786@gmail.com>
2024-06-02 14:42:23 -0700

    lists integration

--- 02-CSS/14 - teamWork/components/app.js
+++ 02-CSS/14 - teamWork/components/app.js
@@ -25,3 +25,11 @@ document.addEventListener('DOMContentLoaded', function () {
     window.addEventListener('scroll', handleScroll);
     handleScroll(); 
 });
+
+document.getElementById('menuButton').addEventListener('click', function() {
+    document.getElementById('slidingMenu').classList.add('open');
+});
+
+document.getElementById('closeMenuButton').addEventListener('click', function() {
+    document.getElementById('slidingMenu').classList.remove('open');
+});
