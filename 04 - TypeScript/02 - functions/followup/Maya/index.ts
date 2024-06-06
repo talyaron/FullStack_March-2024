9aec12524c8535ed8c5ff2eb2cacfd041a118cb0
Mayalvy <mayalevy20@gmail.com>
2024-06-06 19:03:37 +0300

    followup function

---
+++ 04 - TypeScript/02 - functions/followup/Maya/index.ts
@@ -0,0 +1,13 @@
+function division(a:number, b:number): number{
+    return a/b;
+}
+
+console.log(division(5,10));
+console.log(division(100, 50));
+console.log(division(8,40));
+
+function string(a:string, b:string): string{
+    return a + b;
+}
+
+console.log(string(Hello, Maya));
