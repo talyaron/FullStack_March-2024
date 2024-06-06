9f4fedb5cc6ed6d2800835611e13148c264a9bf0
Tal <tal.yaron@gmail.com>
2024-06-06 18:13:31 +0300

    starting TypeScript

---
+++ 04 - TypeScript/variables/example/index.ts
@@ -0,0 +1,28 @@
+//Variables are a way to store data in memory
+
+// Variable declaration
+let x = 10;
+
+x=45;
+
+
+
+console.log(x);
+
+x= 200;
+
+console.log(x); // 200
+
+let y = x;
+
+console.log(y); 
+
+let firstName = "Yossi";
+
+console.log(firstName);
+
+firstName = "Reut";
+console.log(firstName);
+
+console.log(firstName + " " + y);
+console.log(x +  y);
