0ddaa47a59716bd51d11a3909d16ff76ad62ca26
israelcohen1 <134219173+israelcohen1@users.noreply.github.com>
2024-06-06 19:04:12 +0300

    function

---
+++ 04 - TypeScript/02 - functions/followup/israel/index.ts
@@ -0,0 +1,24 @@
+function division (a:number , b: number ) :number{
+    return a/b;
+}
+
+
+console.log(division (100,10));
+
+
+function multiplication (a:number , b: number ) :number{
+    return a*b;
+}
+
+
+console.log(multiplication (100,10));
+
+
+function q (a:number , b: number ) :number{
+    return a**b;
+}
+
+
+console.log(q (100,10));
+
+
