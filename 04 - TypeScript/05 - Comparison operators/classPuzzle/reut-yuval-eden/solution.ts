5b82682322e3fd3975d38d8902bf2be4ae8b509a
YuvalL26 <huckkuh.yl@gmail.com>
2024-06-13 16:04:51 +0300

    operators class

---
+++ 04 - TypeScript/05 - Comparison operators/classPuzzle/reut-yuval-eden/solution.ts
@@ -0,0 +1,19 @@
+function longer_word (word1:string, word2:string): string{
+    try {
+        if(!(typeof(word1)===String) || !(typeof(word2===String)){
+            Error.
+        }
+       if(word1.length > word2.length){
+        return word1
+       }
+       if(word1.length < word2.length){
+        return word2
+       }
+       else{
+        return ('words are at the same length')
+       }
+
+    } catch (error) {
+        
+    }
+}
