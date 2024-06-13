8b4386745090d6f826c71ae6c3fb66d7e55583ee
Tal <tal.yaron@gmail.com>
2024-06-13 18:43:53 +0300

    class puzzle

---
+++ 04 - TypeScript/09 - solving problems/example/index.ts
@@ -0,0 +1,15 @@
+function highestNumber(n1: number, n2: number, n3: number, n4: number): number | undefined {
+    try {
+        
+        if (n1 > n2 && n1> n3 && n1 >n4) {
+            return n1
+        } else if(n2 >n1 && n2 > n3 && n2 > n4){
+           return n2;
+        }
+
+       
+    } catch (error) {
+        console.error(error);
+        return undefined
+    }
+}
