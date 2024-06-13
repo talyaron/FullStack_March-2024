2eeb1059306d22bb13ab661ab08d73147acc3374
ReutKedar <reutkedar@gmail.com>
2024-06-13 20:33:16 +0300

    class puzzle

---
+++ 04 - TypeScript/10 - Arrays - 13-06-2024/followups/Reut/index.ts
@@ -0,0 +1,5 @@
+const numbers: number[]= [0,3,900,56];
+const strings: string[]= ["Lady", "lulu", "luz", "Katanchic", "Mokie", "Cottege"];
+
+console.log (numbers.sort((a,b)=>a-b));
+console.log (strings.length);
