93984c7510a076635d1c285bf5c8f6742224a3cc
ReutKedar <reutkedar@gmail.com>
2024-06-16 12:08:56 +0300

    update homework

--- 04 - TypeScript/10 - Arrays - 13-06-2024/HomePuzzles/Reut/index.ts
+++ 04 - TypeScript/10 - Arrays - 13-06-2024/HomePuzzles/Reut/index.ts
@@ -41,13 +41,47 @@ console.log(findMax([]));
 
 //Write a function filterEven(arr: number[]): number[] that takes an array of numbers and returns a new array containing only the even numbers
 
-function filterEven(arr:number []):number[] | undefined {
+function filterEven(arr: number[]): number[] | undefined {
     try {
-        const evenNumbers = arr.filter(num => num % 2 ===0)    
+        const evenNumbers = arr.filter(num => num % 2 === 0)
         return evenNumbers;
     } catch (error) {
         console.error(error);
         return undefined;
     }
 }
-console.log (filterEven ([1,20,8,9,3,5,6,60,2,-2]));
+console.log(filterEven([1, 20, 8, 9, 3, 5, 6, 60, 2, -2]));
+
+
+//Write a function unique(arr: SomeType[]): SomeType[] that takes an array of any type and returns a new array with only unique elements.
+
+function unique(arr: any[]): any[] {
+    let uniqueArr: any[] = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        let Element = arr[i];
+
+        if (uniqueArr.indexOf(Element) === -1) {
+            uniqueArr.push(Element);
+        }
+    }
+    return uniqueArr;
+}
+console.log(unique(['mango', 'banana', 'melon', 'banana', 'apple']));
+console.log(unique(['mango', 'mango', 'mango', 'mango', 'mango']));
+
+
+// Print the elements of an array in the decreasing frequency if 2 numbers have the same frequency then print the one which came first
+
+function frequencyCounter(arr: number): { [key: number]: number[] } | undefined {
+    const count: { [key: number]: number } = {};
+    for (let i = 0; i < arr.length; i++) {
+        const num = arr[i];
+        if (count[num]) {
+            count[num] ++;
+        } else {
+            count[num] = 1;
+        }
+    }
+}
+console.log(frequencyCounter([5, 8, 5, 5, 8, 2, 1,]));
