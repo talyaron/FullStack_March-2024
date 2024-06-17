015aaf105e3ec6ea16677ef8cb2888d382dfc4fa
crocodama <naamalevi2004@gmail.com>
2024-06-17 19:48:46 +0300

    classpuzzle-update

--- 04 - TypeScript/11 - Array-methods/classPuzzles/room 4/index.ts
+++ 04 - TypeScript/11 - Array-methods/classPuzzles/room 4/index.ts
@@ -30,3 +30,20 @@ function filterWordsByLetter(arr:string[], letter:string){
 }
 const words2 = ["apple", "banana", "cherry", "date","avocado", "pear"];
 console.log(filterWordsByLetter(words2, "e"));
+
+function filterStrings(data:any[]){
+    try {
+        let arr2:number[]=[];
+        data.forEach(element => {
+            if(typeof element === "number"){
+                arr2.push(element);
+            }
+        });
+        arr2.sort((a,b)=>a-b)
+        return arr2;
+    } catch (error) {
+        console.error(error);
+    }
+}
+const words3 = ["apple", "banana", "cherry", "date","avocado", "pear",5,true,107474,false,4.4];
+console.log(filterStrings(words3));
