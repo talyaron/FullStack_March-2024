92ab50bd6a84069e2bff9c427c126be2492f3138
israelcohen1 <134219173+israelcohen1@users.noreply.github.com>
2024-06-20 18:13:34 +0300

    added A few pages

--- 04 - TypeScript/10 - Arrays - 13-06-2024/HomePuzzles/israel/index.ts
+++ 04 - TypeScript/10 - Arrays - 13-06-2024/HomePuzzles/israel/index.ts
@@ -15,10 +15,38 @@ function filterEven (numbers:number[]):number[]{
     try {
        return numbers.filter((Number) => Number %2 === 0);
     } catch (error) {
-        
+
     }
 }
 
 
 console.log(filterEven([1,2,3,4,5,6,7,8,9]));
+
+
+
+function SomeType(array: any[]): any {
+    try {
+        let arraySomeType: any[] = [];
+        array.forEach((array) => {
+            if (typeof array === 'number') {
+                arraySomeType.push(array);
+            }
+        })
+        return arraySomeType;
+    } catch (error) {
+
+    }
+}
+
+
+console.log(SomeType([1, "frt", 45, "jhy", "hjg", 1254, "fhj", 45]));
+
+
+
+function
+
+
+
+
+
 
