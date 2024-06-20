c8cc97ce5d1ada98f4080c170c15908bdc5523a8
ReutKedar <reutkedar@gmail.com>
2024-06-20 18:27:59 +0300

    update

--- 04 - TypeScript/12 - Objects/classPuzzle/Orly-Naama-Yossi-Reut/index.ts
+++ 04 - TypeScript/12 - Objects/classPuzzle/Orly-Naama-Yossi-Reut/index.ts
@@ -5,6 +5,6 @@ const element = document.getElementById("element1");
 console.dir(element);
 
 //print to console log, the second child text content
-console.log(element.children[1].innerHTML);
-console.log(element.children[0].innerHTML);
+console.log(element?.children[1].innerHTML);
+console.log(element?.children[0].innerHTML);
 console.log(element?.textContent?.split('\n')[0]);
