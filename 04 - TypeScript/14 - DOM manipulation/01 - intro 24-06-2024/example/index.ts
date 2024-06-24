29d80e04275a140ada40bb8d6c1ef57c7e4be4e3
Tal <tal.yaron@gmail.com>
2024-06-24 18:08:40 +0300

    followups

--- 04 - TypeScript/14 - DOM manipulation/01 - intro 24-06-2024/example/index.ts
+++ 04 - TypeScript/14 - DOM manipulation/01 - intro 24-06-2024/example/index.ts
@@ -1,3 +1,28 @@
 // catch only one Element. I uses scss selector
-const myH1 = document.querySelector('#myH1');
+const myH1 = document.querySelector('#myH1') as HTMLDivElement;
 console.dir(myH1);
+if(myH1){
+    myH1.innerText = "Hello World!";
+    myH1.style.color = "blue";
+}
+
+//cache some elements
+const ps = document.querySelectorAll('p') as NodeListOf<HTMLParagraphElement>;
+console.dir(ps);
+
+//loop through the elements
+if(ps){
+    ps.forEach(p => {
+        p.style.color = "green";
+    });
+   
+}
+
+const bigs = document.querySelectorAll('.big') as NodeListOf<HTMLParagraphElement>;
+console.dir(bigs);
+
+if(bigs){
+    bigs.forEach(big => {
+        big.style.fontSize = "2rem";
+    });
+}
