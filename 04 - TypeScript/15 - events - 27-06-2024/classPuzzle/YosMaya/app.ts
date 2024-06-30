d44cda3f5d4da14795fa265723eb75598a676116
ShorY <yosishor28@gmail.com>
2024-06-30 12:27:02 +0300

    add animation box

--- 04 - TypeScript/15 - events - 27-06-2024/classPuzzle/YosMaya/app.ts
+++ 04 - TypeScript/15 - events - 27-06-2024/classPuzzle/YosMaya/app.ts
@@ -3,11 +3,13 @@ function handleClicked(event) {
     console.log(event)
     console.log(event.x)
     console.log(event.y)
-    const dom = WriteToDom(event.x,event.y)
+    const dom = WriteToDom(event.x, event.y)
     console.log(dom)
 
 
 }
+
+
 
 function WriteToDom(x: number, y: number): string | undefined {
     try {
@@ -17,9 +19,10 @@ function WriteToDom(x: number, y: number): string | undefined {
         addDiv.style.width = '10px';
         addDiv.style.height = '10px';
         addDiv.style.backgroundColor = 'red';
-        addDiv.style.top = `${y-80}px`;
-        addDiv.style.left = `${x-11}px`;
+        addDiv.style.top = `${y}px`;
+        addDiv.style.left = `${x}px`;
         addDiv.style.position = 'absolute';
+        addDiv.style.transform = 'translate(+50%,+50%)';
         clickEvent?.appendChild(addDiv)
 
         // clickEvent?.innerText += clickEvent;
@@ -43,4 +46,29 @@ function handleChange(event) {
     }
     console.log(text.toUpperCase())
 
+}
+
+function handleClickMakeBigger(event): undefined | void {
+    try {
+        // const buttonId = document.getElementById("buttonId");
+        // buttonId?.style.width += '10px'
+        const button: number = event.target.style.width.split('px')[0] * 1.1;
+        event.target.style.width += button + 'px'
+        console.log(button)
+    } catch (error) {
+        console.error(error);
+        return undefined;
+    }
+
+}
+
+function disappear() {
+    try {
+        const disappear = document.getElementById('catch');
+        disappear?.style.display = 'none'
+        console.log('clicked')
+    } catch (error) {
+        console.error(error);
+        return undefined;
+    }
 }
