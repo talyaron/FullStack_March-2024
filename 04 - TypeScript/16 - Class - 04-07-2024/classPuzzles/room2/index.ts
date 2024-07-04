9348c7d71a08b106c6226a22578a1980dad9646a
crocodama <naamalevi2004@gmail.com>
2024-07-04 20:10:57 +0300

    finished room2

--- 04 - TypeScript/16 - Class - 04-07-2024/classPuzzles/room2/index.ts
+++ 04 - TypeScript/16 - Class - 04-07-2024/classPuzzles/room2/index.ts
@@ -1,4 +1,5 @@
 class Player {
+    id:string
     name:string
     image:string
     yearOfBirth:number
@@ -6,6 +7,7 @@ class Player {
     y:number
 
     constructor(name:string,image:string,yearOfBirth:number,x:number,y:number){
+        this.id = Math.random().toString();
         this.name = name;
         this.image = image;
         this.yearOfBirth = yearOfBirth;
@@ -24,34 +26,41 @@ const ronaldo = new Player("ronaldo","./images/ronaldo.jpg",1985,42,65);
 const thirdGuy = new Player("IDK","./images/thirdguy.jpg",2000,52,10);
 
 const mainDiv = document.querySelector("#field") as HTMLDivElement;
-console.dir(mainDiv);
 function playersDiv(player:Player):string | undefined{
     try {
-        return `<div class='players' style="left:${player.x}%;top:${player.y}%" onclick='${player.moveToRight()}'><img src='${player.image}' alt='${player.name}'></div>`;
+        return `<div class='players' style="left:${player.x}%;top:${player.y}%" onClick='handleMove("${player.id}")'><img src='${player.image}' alt='${player.name}'></div>`;
     } catch (error) {
         console.error(error);
         return undefined;
     }
 }
 const players:Player[]= [messi,ronaldo,thirdGuy];
-function writePlayersToDOM(player:Player[],element:HTMLDivElement){
+function writePlayersToDOM(){
     try {
-        if (!element) throw new Error("Element not found");
-        if (!player) throw new Error("No player found");
+        if (!mainDiv) throw new Error("Element not found");
+        if (!players) throw new Error("No player found");
         let html:string = ``;
-        player.forEach(player => {
+        players.forEach(player => {
             const htmlplayer = playersDiv(player);
            if(htmlplayer) html += htmlplayer;
         });
 
-        element.innerHTML = html;
+        mainDiv.innerHTML = html;
     } catch (error) {
         console.error(error);
         return undefined;
     }
 }
 
-writePlayersToDOM(players,mainDiv);
-function updatePos(player:Player){
-    
+writePlayersToDOM();
+function handleMove(id:string){
+    try {
+        const player = players.find(p=>p.id===id);
+        if (!player) throw new Error("player not found");
+        player.moveToRight();
+        writePlayersToDOM();
+    } catch (error) {
+        console.error(error);
+        return undefined;
+    }
 }
