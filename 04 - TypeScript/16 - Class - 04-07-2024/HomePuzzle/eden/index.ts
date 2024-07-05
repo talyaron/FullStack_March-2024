318c8977ddc9202acb3b221f84891218c8ec9f99
edenshaviv <139482652+edenshaviv@users.noreply.github.com>
2024-07-05 20:49:39 +0300

    5.7

---
+++ 04 - TypeScript/16 - Class - 04-07-2024/HomePuzzle/eden/index.ts
@@ -0,0 +1,12 @@
+class players {
+    constructor(type:string, name:string, imageUrl: String; , team: string, age: number)  {
+        this.type = type;
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.team = team;
+        this.age = age;
+
+    }
+   
+
+}
