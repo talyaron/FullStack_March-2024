3398b862e979fec05ef9d4294f3d22213f624345
Tal Yaron <tal.yaron@gmail.com>
2024-07-08 20:42:29 +0300

    solved

--- 04 - TypeScript/17 - MVC-2 - 08-07-2024/homePuzzles/talY/tal-project/src/main.ts
+++ 04 - TypeScript/17 - MVC-2 - 08-07-2024/homePuzzles/talY/tal-project/src/main.ts
@@ -1,7 +1,13 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
+import './style.scss'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
+import flower from './assets/images/flower.jpg'
+import { Flower, flowers } from './model/flowerModel.ts'
+import { renderFlower } from './view/flower/Flower.ts'
+
+flowers.push(new Flower(flower, 'flower1'),new Flower(flower, 'flower1'));
+const flowersString = flowers.map(flower => renderFlower(flower)).join('');
+console.log(flowersString)
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -9,8 +15,9 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
     <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+      <img src="${flower}" class="logo vanilla" alt="TypeScript logo" />
     </a>
+    ${flowersString}
     <h1>Vite + TypeScript</h1>
     <div class="card">
       <button id="counter" type="button"></button>
