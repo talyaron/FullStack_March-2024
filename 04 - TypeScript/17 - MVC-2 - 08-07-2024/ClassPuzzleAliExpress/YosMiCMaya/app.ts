26701a6bc80382dba828037b7c82b2cec5a7d217
ShorY <yosishor28@gmail.com>
2024-07-08 19:14:41 +0300

    add item to buyer

--- 04 - TypeScript/17 - MVC-2 - 08-07-2024/ClassPuzzleAliExpress/YosMiCMaya/app.ts
+++ 04 - TypeScript/17 - MVC-2 - 08-07-2024/ClassPuzzleAliExpress/YosMiCMaya/app.ts
@@ -2,15 +2,15 @@ class Item {
     name: string;
     price: number;
     id: string;
-    inStock: boolean = true;
+    inStock: boolean;
 
-    constructor(name: string, price: number) {
+    constructor(name: string, price: number, inStock: boolean = true) {
         this.name = name;
         this.price = price;
         this.id = crypto.randomUUID();
+        this.inStock = inStock;
     }
 }
-
 
 class Buyer {
     name: string;
@@ -20,22 +20,26 @@ class Buyer {
     constructor(name: string) {
         this.name = name;
         this.id = crypto.randomUUID();
+        this.cart = []; 
     }
 
     addItem(id: string) {
-        this.cart.push(id)
+        this.cart.push(id);
     }
 }
 
 const item1 = new Item('Soup', 12.34);
 const item2 = new Item('Bag', 9.34);
 const item3 = new Item('Pillow', 14.34);
-const items = [item1, item2, item3]
+const items = [item1, item2, item3];
 
 const maya = new Buyer('Maya');
 const michael = new Buyer('Michael');
 const yos = new Buyer('Yos');
-const buyers = [maya, michael, yos]
-maya.addItem(item1.id)
+const buyers = [maya, michael, yos];
 
-console.log(buyers)
+maya.addItem(item1.id);
+maya.addItem(item2.id);
+
+console.log(buyers);
+console.log(items);
