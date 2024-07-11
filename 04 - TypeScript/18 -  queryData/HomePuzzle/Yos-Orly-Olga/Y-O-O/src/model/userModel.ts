aad1e3f0fc9ebaf2bdecdb37b8a7c8b481042314
olga.larin <21apr1988@gmail.com>
2024-07-11 20:30:14 +0300

    commit after lesson

---
+++ 04 - TypeScript/18 -  queryData/HomePuzzle/Yos-Orly-Olga/Y-O-O/src/model/userModel.ts
@@ -0,0 +1,19 @@
+import { Item } from "../model/itemModel";
+
+export class User {
+    name: string
+    email: string
+    password: string
+    itemsInCart: Item[] = []
+    constructor(name: string, email: string, password: string) {
+        this.name = name
+        this.email = email
+        this.password = password
+    }
+}
+
+export const users: User[] = [
+    new User('Yos', 'yos@yos', '12345'),
+    new User('Orly', 'orly@orly', '12345'),
+    new User('Olga', 'olga@olga', '12345'),
+]
