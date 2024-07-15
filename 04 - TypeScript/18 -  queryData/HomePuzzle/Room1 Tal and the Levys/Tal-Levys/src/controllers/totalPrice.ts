14dfc17a291fd50bc6f03b517e1f7312eec3a6e8
shohat777 <143879775+shohat777@users.noreply.github.com>
2024-07-15 11:13:03 +0300

    the function works.
    maybe a change to the variable and class name are required to fit the rest of the code.

--- 04 - TypeScript/18 -  queryData/HomePuzzle/Room1 Tal and the Levys/Tal-Levys/src/controllers/totalPrice.ts
+++ 04 - TypeScript/18 -  queryData/HomePuzzle/Room1 Tal and the Levys/Tal-Levys/src/controllers/totalPrice.ts
@@ -1,42 +1,39 @@
 // total price of array(?) of items, for each item double item.price by item.quantity  *only items and the quantity that was moved to the cart.
 
-class cartItem{
-    id:string;
-    name: string;
-    price: number;
-    quantity:number;
+class CartItem {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
 
-    constructor (id:string, name:string, price:number, quantity:number){
-        this.id=id,
-        this.name=name,
-        this.price=price
-        this.quantity=quantity
-    }
+  constructor(id: string, name: string, price: number, quantity: number) {
+    (this.id = id), (this.name = name), (this.price = price);
+    this.quantity = quantity;
+  }
 }
 
-const bags:cartItem[] = [
-    new cartItem("1","bag",150,5),
-    new cartItem("2","bag2",30,10),
-    new cartItem("3","bag3",65,7),
-    new cartItem("4","bag4",50,2),
-    new cartItem("5","bag5",100,6)];
+const userCart: CartItem[] = [
+  new CartItem("1", "bag", 150, 5),
+  new CartItem("2", "bag2", 30, 10),
+  new CartItem("3", "bag3", 65, 7),
+  new CartItem("4", "bag4", 50, 2),
+  new CartItem("5", "bag5", 100, 6),
+];
+
+console.log(userCart);
+
+function getTotalPrice(userCart: CartItem[]) {
+  let sum = 0;
+  try {
+    userCart.forEach((CartItem) => {
+      sum += CartItem.quantity * CartItem.price;
+    });
+    return sum;
+  } catch (error) {
+    console.error(error);
+    return 0;
+  }
+}
+console.log(getTotalPrice(userCart));
 
 
-    console.log(bags);
-    
-    
-    function getTotalPrice(bags :cartItem[],) {
-        let sum=0 
-        try {
-            bags.forEach ( cartItem => {sum+= cartItem.price }  )
-            return sum
-        } catch (error) {
-            console.error(error);
-            return []
-        }
-    }
-    console.log(getTotalPrice(bags));
-    
-    
-    // if the items in the cart were : 
-    // from bag2 - 7 items, from bag5 - 2 items and from bag3 - 4 items.  
