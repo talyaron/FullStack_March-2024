f1723ba1e2d6ac8dd498fce312c44bbc9a29cfdd
ShorY <yosishor28@gmail.com>
2024-07-18 19:17:58 +0300

    form examples

--- 04 - TypeScript/19 - forms - 18-07-2024/followups/yos/app.ts
+++ 04 - TypeScript/19 - forms - 18-07-2024/followups/yos/app.ts
@@ -15,7 +15,7 @@ function handleClick(event: Event) {
 }
 
 
-function handleMouseOut(event:Event){
+function handleMouseOut(event: Event) {
     try {
         console.log(event?.target.value)
     } catch (error) {
@@ -23,12 +23,37 @@ function handleMouseOut(event:Event){
     }
 }
 
-function handleChangeNumber(event:Event){
+function handleChangeNumber(event: Event) {
     try {
         const value = event.target.value
-        if(value > 2 ){
+        if (value > 2) {
             alert("Number must be less than 2")
         }
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+function handleSubmit(event: Event) {
+    try {
+        event.preventDefault()
+        console.log(event.target)
+        let features = event.target.features
+        console.dir(features)
+        let selectedCheckbox = []
+        features.forEach(element => {
+            if(element.checked)
+                selectedCheckbox.push(element.value)
+        });
+    
+        console.log(selectedCheckbox)
+        console.log(event!.target['name']!.value)
+        console.log(event.target.email.value)
+        console.log(event!.target.checkbox!.checked)
+        console.log(event!.target.select!.value)
+        const form = event!.target as HTMLFormElement
+        form.reset()
+
     } catch (error) {
         console.error(error)
     }
