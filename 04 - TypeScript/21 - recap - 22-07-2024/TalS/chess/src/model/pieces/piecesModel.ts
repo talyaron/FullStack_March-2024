80261c39a0a311a01ce55d73e8d9a5cefab4ee28
shohat777 <143879775+shohat777@users.noreply.github.com>
2024-07-25 17:17:47 +0300

    .

---
+++ 04 - TypeScript/21 - recap - 22-07-2024/TalS/chess/src/model/pieces/piecesModel.ts
@@ -0,0 +1,27 @@
+import { pawn, Position, Type } from "../types/typesModel";
+
+export class Piece{
+    id:string;
+    color:"black"|"white";
+    position:Position;
+    type:Type
+
+    constructor(color:"black"|"white", position:Position, type:Type){
+        this.id = crypto.randomUUID()
+        this.position = position;
+        this.color = color;
+        this.type = type;
+    }
+
+    // method: setNewPosition("pos-0b") ->  new position
+    function setNewPosition(params:type) {
+        
+    }
+}
+
+export const pieces:Piece[] = [];
+
+
+
+const pawn1 = new Piece("black",new Position(3,5),pawn )
+pieces.push(pawn1)
