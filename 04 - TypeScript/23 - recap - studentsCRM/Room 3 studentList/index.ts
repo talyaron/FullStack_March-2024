ea90faaecfe224682c84d928fd1480b7f65ca7ca
shohat777 <143879775+shohat777@users.noreply.github.com>
2024-07-29 20:26:56 +0300

    started level 2

--- 04 - TypeScript/23 - recap - studentsCRM/Room 3 studentList/index.ts
+++ 04 - TypeScript/23 - recap - studentsCRM/Room 3 studentList/index.ts
@@ -1,7 +1,8 @@
 class Student {
   name: string;
   id: string;
-  constructor(name: string) {
+  grades: Grade[];
+  constructor(name: string ) {
     this.name = name;
     this.id = `id-${crypto.randomUUID()}`;
   }
@@ -9,12 +10,24 @@ class Student {
 
 const students: Student[] = [];
 
+class Grade {
+    grade: number;
+    subject: string;
+    constructor(grade: number, subject: string ) {
+        this.grade = grade;
+        this.subject = subject;
+    }
+}
+
+const grades: Grade [] = [];
+
 function handleAddStudent(ev: any) {
   ev.preventDefault();
   try {
     const form = ev.target.student.value;
     students.push(new Student(form));
     renderStudents(students);
+    ev.target.reset();
   } catch (error) {
     console.error(error);
   }
@@ -26,19 +39,15 @@ function renderStudents(students: Student[]) {
     students.forEach((student) => {
       html += `<li class = "student"> 
         ${student.name}
-        <form onsubmit = "handleAddGrades"
+        <form onsubmit = "handleAddGrades(event)">
+        <input
         </li>`;
     });
     html += "</ol>";
-    const studentList = document.querySelector(
-      "#studentsList"
-    ) as HTMLDivElement;
+    const studentList = document.querySelector("#studentsList") as HTMLDivElement;
     if (!studentList) throw new Error("List not found");
-
     studentList.innerHTML = html;
   } catch (error) {
     console.error(error);
   }
 }
-
-
