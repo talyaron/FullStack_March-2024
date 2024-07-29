54fef1c8ecdee5585fc4e39b0166ae8c7d724bbd
Roni-Hoshen <ronihoshen100@gmail.com>
2024-07-29 20:25:36 +0300

    room5

--- 04 - TypeScript/23 - recap - studentsCRM/Room4/studentModle.ts
+++ 04 - TypeScript/23 - recap - studentsCRM/Room4/studentModle.ts
@@ -1,12 +1,24 @@
 class Student {
-    name: string;
-    id: string;
-constructor (name:string){
-    this.name = name,
+  name: string;
+  id: string;
+  grades: Grade[];
+  constructor(name: string) {
+    this.name = name;
     this.id = `id-${crypto.randomUUID()}`;
+    this.grades = [];
+  }
+  addGrade (grade:number, subject:string){
+    this.grades.push(new Grade(grade,subject))
+  }
 }
-
+class Grade{
+    grade: number;
+    subject: string;
+    id: string;
+    constructor(grade:number, subject:string){
+        this.grade = grade;
+        this.subject = subject;
+        this.id = `id-${crypto.randomUUID()}`;
+    }
 }
- const students:Student[]=[];
-
- 
+const students: Student[] = [];
