7c6dd48e48b6dbf47a5854f28b8f2e39fd3948de
YuvalL26 <45969001+YuvalL26@users.noreply.github.com>
2024-07-31 17:50:59 +0300

    yuval

---
+++ yuval-mvc-practice/studentsDataBase/src/model/studentModel.ts
@@ -0,0 +1,13 @@
+export class Student{
+    id:string
+    name:string
+    grades:Grade[]
+
+    constructor(name:string){
+        this.id = crypto.randomUUID();
+        this.name = name;
+        this.grades=[];
+    }
+}
+
+export const students:Student[]=[];
