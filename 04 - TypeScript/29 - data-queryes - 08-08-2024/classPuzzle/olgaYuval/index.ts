6bf26327c206f18a0e698d0d05f5bb20e327c848
YuvalL26 <45969001+YuvalL26@users.noreply.github.com>
2024-08-08 20:00:52 +0300

    olga yossi yuval

--- 04 - TypeScript/29 - data-queryes - 08-08-2024/classPuzzle/olgaYuval/index.ts
+++ 04 - TypeScript/29 - data-queryes - 08-08-2024/classPuzzle/olgaYuval/index.ts
@@ -2,11 +2,15 @@ class Product {
     name: string;
     price: number;
     id:string;
-    constructor(name:string, price:number) {
+    quantity:number;
+    constructor(name:string, price:number, quantity:number) {
         this.name = name;
         this.price = price;
+        this.quantity= quantity;
         this.id = Math.random().toString(16).slice(2);
     }
+
+
 }
 
 const products:Product[] = []
@@ -14,7 +18,7 @@ const products:Product[] = []
 class Customer {
     name: string;
     id:string;
-    
+    productsIdList:string[] = [];
     constructor(name:string) {
         this.name = name;
         this.id = Math.random().toString(16).slice(2);
