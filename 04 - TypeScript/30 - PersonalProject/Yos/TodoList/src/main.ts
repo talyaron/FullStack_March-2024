2292b450120a4cbe2d8d4c7e9c541a981ce367b8
ShorY <yosishor28@gmail.com>
2024-08-19 17:58:13 +0300

    remove my project

--- 04 - TypeScript/30 - PersonalProject/Yos/TodoList/src/main.ts
+++
@@ -1,5 +0,0 @@
-import './style.css'
-import { renderLogin } from './controllers/login';
-
-const app = document.querySelector<HTMLDivElement>('#todo-list')! as HTMLDivElement;
-if(app) renderLogin(app);
