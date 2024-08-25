1495e242ad46a2dd1f6cff17da98f21f6b1f20be
ShorY <yosishor28@gmail.com>
2024-08-25 16:42:55 +0300

    update yos todo project

--- 04 - TypeScript/30 - PersonalProject/Yos/TodoList/src/main.ts
+++ 04 - TypeScript/30 - PersonalProject/Yos/TodoList/src/main.ts
@@ -1,10 +1,5 @@
-import './style.css'
+import './views/styles/root.scss'
 import { renderLogin } from './controllers/login';
-import  {showPopUpFinishYourTask} from './controllers/popup';
 
 const app = document.querySelector<HTMLDivElement>('#todo-list')! as HTMLDivElement;
-if(app) renderLogin(app);
-
-
-//run set time interval to run every minute
-setInterval(showPopUpFinishYourTask, 10000);
+if (app) renderLogin(app);
