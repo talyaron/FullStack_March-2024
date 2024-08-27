79273da4f8208c6fa00d5e686660a8ec2aa4273c
Mayalvy <mayalevy20@gmail.com>
2024-08-27 19:37:46 +0300

    model update

--- 04 - TypeScript/35 - TeamProject/Mikhail&Maya/Shooting-game/src/Model/TargetModel.ts
+++ 04 - TypeScript/35 - TeamProject/Mikhail&Maya/Shooting-game/src/Model/TargetModel.ts
@@ -1,17 +1,33 @@
 export class Target {
     image: string;
     position: { x: number; y: number };
-    id:string;
+    id: string;
     direction: boolean;
+    isHit: boolean;
 
-    constructor(image: string, position: { x: number; y: number }, ){
-        this.image = image,
-        this.position = position,
-        this.id = crypto.randomUUID(),
-        this.direction = true
-    }
-    setposition(position: { x: number; y: number }){
+    constructor(image: string, position: { x: number; y: number }) {
+        this.image = image;
         this.position = position;
+        this.id = crypto.randomUUID();
+        this.direction = true;
+        this.isHit = false;
+    
     }
-}
+    move() {
+        if (this.direction) {
+            this.position.x += 5; // Move right
+            if (this.position.x >= 800) this.direction = false; // Reverse direction
+        } else {
+            this.position.x -= 5; // Move left
+            if (this.position.x <= 0) this.direction = true; // Reverse direction
+        }
+    }
+        hit() {
+            this.isHit = true;
+        }
+    }
+    // setposition(position: { x: number; y: number }){
+    //     this.position = position;
+    // }
+
 
---
+++ 04 - TypeScript/35 - TeamProject/Mikhail&Maya/Shooting-game/src/View/homescreen.ts
@@ -0,0 +1,2 @@
+export function renderHomeScreen(targets: Target[], weapon: Weapon) {
+}
