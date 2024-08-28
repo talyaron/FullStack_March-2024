b7e40384f2120f2184f3784d2a6881ebf08f7768
HotNuggets <mishamandark@gmail.com>
2024-08-28 17:47:53 +0300

    update

--- 04 - TypeScript/35 - TeamProject/Mikhail&Maya/Shooting-game/src/View/startbutton.ts
+++ 04 - TypeScript/35 - TeamProject/Mikhail&Maya/Shooting-game/src/View/startbutton.ts
@@ -1,10 +1,17 @@
-import { Target } from "../Model/TargetModel";
-import { Weapon } from "../Model/WeaponModel";
+
 import { renderWeaponSelect } from "./weaponSelect";
 
 
 export function renderStart(root:HTMLElement) {
+ 
     root.innerHTML = `<div class="start_button">
-      <button id="start" type="button" onclick="renderWeaponSelect(root)">Start game</button>
+      <button id="start" type="button"">Start game</button>
   </div>
-`;}
+`;
+
+
+const startButton = document.getElementById('start');
+if (startButton) {
+    startButton.addEventListener('click', () => renderWeaponSelect(root));
+}
+}
--- 04 - TypeScript/35 - TeamProject/Mikhail&Maya/Shooting-game/src/View/weaponSelect.ts
+++ 04 - TypeScript/35 - TeamProject/Mikhail&Maya/Shooting-game/src/View/weaponSelect.ts
@@ -1,10 +1,16 @@
+import { handleStartGame } from "../Controllers/handleStartGame";
 import { renderStart } from "./startbutton";
 export function renderWeaponSelect(root:HTMLElement){
-    
+    console.log("hello world");
     root.innerHTML=`<div class="weaponSelect">
     <div class="weaponCard">
-    <img src="./images/pistol.jpg" onclick="handleStartGame()>
+    <img src="./images/pistol.jpg">
     </div>
     </div>`;
      
+    const startGameImg = document.getElementById('startGame');
+    if (startGameImg) {
+        startGameImg.addEventListener('click', handleStartGame);
+    }
+
 }
