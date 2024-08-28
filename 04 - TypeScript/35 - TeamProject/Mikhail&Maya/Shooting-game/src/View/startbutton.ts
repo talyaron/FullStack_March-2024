85895ec7d3fc9254c9972970cb840db561b8f3c7
HotNuggets <mishamandark@gmail.com>
2024-08-28 16:40:42 +0300

    update

--- 04 - TypeScript/35 - TeamProject/Mikhail&Maya/Shooting-game/src/View/startbutton.ts
+++ 04 - TypeScript/35 - TeamProject/Mikhail&Maya/Shooting-game/src/View/startbutton.ts
@@ -1,10 +1,11 @@
 import { Target } from "../Model/TargetModel";
 import { Weapon } from "../Model/WeaponModel";
+import { renderWeaponSelect } from "./weaponSelect";
 
 
-export function renderStart() {
-    document.querySelector<HTMLDivElement>('#page')!.innerHTML = `
+export function renderStart(root:HTMLElement) {
+    root.innerHTML = `
   <div class="start_button">
-      <button id="start" type="button" onclick="renderWeaponSelect()">Start game</button>
+      <button id="start" type="button" onclick="renderWeaponSelect(root)">Start game</button>
   </div>
-`}
+`;}
--- 04 - TypeScript/35 - TeamProject/Mikhail&Maya/Shooting-game/src/View/weaponSelect.ts
+++ 04 - TypeScript/35 - TeamProject/Mikhail&Maya/Shooting-game/src/View/weaponSelect.ts
@@ -1,4 +1,4 @@
-export function renderWeaponSelect(root:HTMLElement){
+export function renderWeaponSelect(){
     let html='<div class="weaponSelect"><div class="weaponCard"><img src="./images/pistol.jpg" onclick="handleStartGame()></div></div>';
     root.innerHTML = html;
 }
