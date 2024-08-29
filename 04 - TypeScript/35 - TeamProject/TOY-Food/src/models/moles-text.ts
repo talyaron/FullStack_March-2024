be7d2fe00105d8506af58e4c34b86166d1a52a94
olga.larin <21apr1988@gmail.com>
2024-08-29 18:04:07 +0300

    text models

---
+++ 04 - TypeScript/35 - TeamProject/TOY-Food/src/models/moles-text.ts
@@ -0,0 +1,29 @@
+export class User {
+    id: string = 'id-' + crypto.randomUUID();
+    name: string;
+    mail: string;
+    password: string;
+    itemsInCartDict: any;
+
+    constructor(name: string) {
+        this.name = name;
+        this.mail = "";
+        this.password = "";
+    }
+}
+
+export class Item {
+    id: string = 'id-' + crypto.randomUUID();
+    name: string;
+    price: number;
+    description: string;
+    image: string;
+
+    constructor(name: string, price: number, description: string, image: string) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.image = image;
+    }
+}
+
