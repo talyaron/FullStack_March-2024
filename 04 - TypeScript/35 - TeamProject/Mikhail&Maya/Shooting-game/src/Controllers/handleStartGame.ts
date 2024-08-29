bd602953929d7af49abb10082f35e098d8708232
HotNuggets <mishamandark@gmail.com>
2024-08-29 13:55:52 +0300

    update

--- 04 - TypeScript/35 - TeamProject/Mikhail&Maya/Shooting-game/src/Controllers/handleStartGame.ts
+++ 04 - TypeScript/35 - TeamProject/Mikhail&Maya/Shooting-game/src/Controllers/handleStartGame.ts
@@ -1,3 +1,12 @@
+import { Weapon } from "../Model/WeaponModel"
 export function handleStartGame(weaponId:string){
+  switch(weaponId){
+
+    case 'pistolImage':
+        const weapon=new Weapon('/src/images/weapons/pistolSelect.png','/src/audio/pew.m4a',weaponId);
+        break;
+  }
+  
+  
     
 }
