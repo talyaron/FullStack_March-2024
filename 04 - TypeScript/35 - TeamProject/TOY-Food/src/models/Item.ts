5fef66d180c755a961caa7ef3abee35b0cf70e3e
Tanya1786 <tanya1786@gmail.com>
2024-08-30 18:32:21 -0700

    menu and some content added

---
+++ 04 - TypeScript/35 - TeamProject/TOY-Food/src/models/Item.ts
@@ -0,0 +1,21 @@
+export enum ItemCategory {
+  Appetizer = "Appetizer",
+  Main = "Main",
+  Dessert = "Dessert",
+  Drink = "Drink"
+}
+
+export class Item {
+  public id: string;
+  public category: ItemCategory;
+  public pic: string;
+  public name: string;
+  public price: number;
+  constructor(category: ItemCategory, pic: string, name: string, price: number) {
+    this.id = `id${crypto.randomUUID()}`;
+    this.category = category;
+    this.pic = pic;
+    this.name = name;
+    this.price = price;
+  }
+}
--- 04 - TypeScript/35 - TeamProject/TOY-Food/src/views/HeaderView.ts
+++ 04 - TypeScript/35 - TeamProject/TOY-Food/src/views/HeaderView.ts
@@ -1,62 +1,34 @@
+import { Cart } from '../models/Cart';
+import { Item, ItemCategory } from '../models/Item';
+
 export function renderHeader() {
-    const app = document.querySelector("#app");
-    const header = document.createElement("header");
-    header.classList.add("header");
-    header.innerHTML = `
-        <div class="header__nav">
-            <div class="header__logo">
-                <img src="./src/assets/images/logo-text.png" alt="logo">
-            </div>
-            <div class="header__menu">
-                <a href="#home">Home</a>
-                <a href="#about">Menu</a>
-                <a href="#contact">Contact</a>
-            </div>
-            <div class="header__icons">
-                <i class='bx bx-search'></i>
-                ${renderHeaderUser({ name: "User", itemsInCartDict: undefined })}
-            </div>
-        </div>
-    `;
-    app!.appendChild(header);
-}
-
-function renderHeaderUser(user: User) {
-    const html = `
-
-            <div class="header__user__name">
-                <i class='bx bx-user'></i> Hello, ${user.name}
-            </div>
-            <div class="header__user__cart">
-                <i class='bx bx-cart'></i>
-                <span>${user.itemsInCartDict !== undefined ? Object.keys(user.itemsInCartDict).length : 0}</span>
-            </div>
-
-    `;
-    return html;
-}
-
-class User {
-    name: string;
-    itemsInCartDict: any;
-
-    constructor(name: string, itemsInCartDict: any) {
-        this.name = name;
-        this.itemsInCartDict = itemsInCartDict;
+    const header = document.querySelector("#header");
+    if (!header) {
+        console.error('No element with id "header" found.');
+        return;
     }
-}
 
-function renderSearchBar() {
-    const html = `
-        <div class="header__search_bar">
-            <input type="checkbox" name="checkbox" id="searchOne">
-            <label for="searchOne">
-                <i class='bx bx-search icon'></i>
-                <i class='bx bx-x' last icon></i>
-                <p>|</p>
-            </label>
-            <input type="text" name="search" id="search" placeholder="Search...">
+    header.innerHTML = `
+        <div class="header">
+            <div class="header__nav">
+                <div class="header__logo">
+                    <img src="./src/assets/images/logo-text.png" alt="logo">
+                </div>
+                <div class="header__menu">
+                    <a href="#home">Home</a>
+                    <a href="#about">Menu</a>
+                    <a href="#contact">Contact</a>
+                </div>
+                <div class="header__icons">
+                    <div class="header__search_bar">
+                        <input type="text" id="search-input" placeholder="Search...">
+                        <label for="search-input">
+                            <i class='bx bx-search'></i>
+                            <p class="last">Search</p>
+                        </label>
+                    </div>
+                </div>
+            </div>
         </div>
     `;
-    return html;
 }
