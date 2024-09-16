f45b680484a9d87dc0397dee2d3c03c686a9f6ab
HotNuggets <mishamandark@gmail.com>
2024-09-16 17:58:19 +0300

    update

--- 05-node-js/04-simple routes and fetch/homePuzzle/mikhail/public/controller/handleList.ts
+++
@@ -1,9 +0,0 @@
-import { todo } from "../model/todo";
-import { todos } from "../model/todos";
-import { renderToDo } from "../view/renderTodo";
-export function handleList(array:todo[]){
-    
-    
-        renderToDo(array);
-   
-}
--- 05-node-js/04-simple routes and fetch/homePuzzle/mikhail/public/model/todos.ts
+++ 05-node-js/04-simple routes and fetch/homePuzzle/mikhail/public/model/todos.ts
@@ -1,13 +1,1 @@
-import { todo } from "./todo";
 
-export class todos{
-    title:string;
-    array:todo[];
-
-    constructor (array:todo[],title:string){
-        this.title=title
-        this.array=array;
-    }
-
-
-}
