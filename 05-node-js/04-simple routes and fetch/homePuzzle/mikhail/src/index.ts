423f796aeafb40c4459cf70783f015228d092564
HotNuggets <mishamandark@gmail.com>
2024-09-18 14:09:49 +0300

    update

--- 05-node-js/04-simple routes and fetch/homePuzzle/mikhail/src/index.ts
+++ 05-node-js/04-simple routes and fetch/homePuzzle/mikhail/src/index.ts
@@ -2,6 +2,55 @@ import express from 'express'
 const app = express()
 const port = 3000
 
+class Todo {
+  id: string;
+  progress: string;
+  title: string;
+  description: string;
+
+  constructor(id:string,progress:string, title: string, description: string) {
+    this.progress = progress;
+    this.id = id;
+    this.title = title;
+    this.description = description;
+  }
+
+  setDescription(description: string) {
+    this.description = description;
+  }
+
+  setTitle(title: string) {
+    this.title = title;
+  }
+  
+  setProgress(progress: string) {
+    this.progress = progress;
+  }
+}
+const tasks:Todo[]=[];
+
+
+
+
+const  addTask = (req: any, res: any) => {
+  try {
+    console.log(req.body);
+    const { id, progress,title,description } = req.body;
+    if (!id || !progress || !title ||!description) throw new Error("task error");
+
+    const newTask = new Todo(id,progress,title,description);
+    tasks.push(newTask);
+
+    res.send({ ok: true })
+  } catch (error:any) {
+    console.error(error)
+    res.send({ ok: false, error: error.message })
+  }
+}
+app.post("/add-student",addTask)
+
+
+
 
 app.use(express.static('public'))
 app.get('/', (req:any, res:any) => {
