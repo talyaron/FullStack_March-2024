8d4870953ae522b09897a274e7a36134a9311139
crocodama <naamalevi2004@gmail.com>
2024-09-19 20:17:38 +0300

    update

--- 05-node-js/07-recap-project/Naama/src/routes/petsRoutes.ts
+++ 05-node-js/07-recap-project/Naama/src/routes/petsRoutes.ts
@@ -1,7 +1,9 @@
 import express from 'express'
 import { getallpets } from '../controllers/pets/getpetscont'
+import { addpet } from '../controllers/pets/setpetscont'
 const router = express.Router()
 
 router.get("/get-all-pets",getallpets)
+router.post("/add-pet",addpet)
 
 export default router
