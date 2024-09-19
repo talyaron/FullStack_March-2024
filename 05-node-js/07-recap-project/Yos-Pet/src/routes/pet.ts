bd8fad150523391084268d779a8c5aabf7a142ea
ShorY <yosishor28@gmail.com>
2024-09-19 18:57:39 +0300

    add post pet

--- 05-node-js/07-recap-project/Yos-Pet/src/routes/pet.ts
+++ 05-node-js/07-recap-project/Yos-Pet/src/routes/pet.ts
@@ -1,14 +1,21 @@
-import { Pet, pets } from "../models/pet";
+import { addPet, allPets } from "../controllers/pets";
 
 const petRouter = require('express').Router();
 
-const allPets:Pet[] = pets;
 
-
-petRouter.get('/all-pets', (req:any, res:any) => {
+petRouter.get('/all-pets', (req: any, res: any) => {
     console.log('all pets');
     res.send(allPets);
 })
 
+petRouter.post('/add-pet', (req: any, res: any) => {
+    console.log('add pet');
+    let { name, age, species, price, imageUrl } = req.query;
+    age = parseInt(age);
+    price = parseInt(price);
+    console.log(name, age, species, price, imageUrl);
+    const allPets = addPet(name, age, species, price, imageUrl);
+    res.send({ 'allPets': allPets });
+})
 
 module.exports = petRouter;
