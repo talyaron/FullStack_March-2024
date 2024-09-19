c3102cb9140ab2456c722f988ee95a950f1ffc26
shohat777 <143879775+shohat777@users.noreply.github.com>
2024-09-19 18:30:26 +0300

    .

--- 05-node-js/07-recap-project/TalS/src/server.ts
+++ 05-node-js/07-recap-project/TalS/src/server.ts
@@ -3,20 +3,7 @@ import { randomUUID } from "crypto"
 const express = require('express')
 const app = express()
 const port = 3000
-class Pets {
-  id:string;
-  name:string;
-  species:string;
-  age:number;
-  price:number;
-  constructor(name:string, species:string, age:number, price:number) {
-    this.id=crypto.randomUUID();
-    this.name=name;
-    this.species=species;
-    this.age=age;
-    this.price=price;
-  }
-}
+
 
 app.get('/', (req:any, res:any) => {
   res.send('Hello World!')
