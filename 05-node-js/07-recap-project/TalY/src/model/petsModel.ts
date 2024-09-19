89302923f4f26ff3c935ee8528a888b4a5c1f6e4
Tal Yaron <tal.yaron@gmail.com>
2024-09-19 20:16:17 +0300

    wip

--- 05-node-js/07-recap-project/TalY/src/model/petsModel.ts
+++ 05-node-js/07-recap-project/TalY/src/model/petsModel.ts
@@ -3,11 +3,8 @@ export class Pet{
     yearOfBirth: number;
     imageUrl?: string;
 
-    constructor( public name: string, public species: string, public age: number, public price: number){
-        this.name = name;
-        this.species = species;
+    constructor( public name: string, public species: string, age: number, public price: number){
         this.yearOfBirth = new Date().getFullYear() - age;
-        this.price = price;
         this.id = crypto.randomUUID();
     }
 }
