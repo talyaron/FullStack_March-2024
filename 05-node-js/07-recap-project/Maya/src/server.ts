5cd98a09dfc92acd818f4c1fc9d486743f02d8bf
Mayalvy <mayalevy20@gmail.com>
2024-09-19 17:59:07 +0300

    class

--- 05-node-js/07-recap-project/Maya/src/server.ts
+++ 05-node-js/07-recap-project/Maya/src/server.ts
@@ -1,3 +1,5 @@
+import { S } from "vite/dist/node/types.d-aGj9QkWt";
+
 console.log('Hello, TypeScript!');
 const express = require('express')
 const app = express()
@@ -11,3 +13,16 @@ app.use(express.static('public'))
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+class Pet{
+Id: String;
+species: String
+age: number;
+price: number;
+constructor( species:String, age:number,price:number){
+this.Id = Math.random().toString();
+  this.species = species;
+  this.age = age;
+  this.price = price;
+}
+}
