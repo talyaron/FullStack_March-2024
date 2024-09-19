c0ebaef008fba772fa7716636694d99b0dcb44e6
crocodama <naamalevi2004@gmail.com>
2024-09-19 18:39:59 +0300

    update

--- 05-node-js/07-recap-project/Naama/src/server.ts
+++ 05-node-js/07-recap-project/Naama/src/server.ts
@@ -1,24 +1,11 @@
 import express from 'express'
+import petsRouter from './routes/petsRoutes'
 const app = express()
 const port = 3000
 
 app.use(express.static('public'))
 
-class Pet{
-  id:string;
-  name:string;
-  species:string;
-  age:number;
-  price:number;
-
-  constructor(name:string,species:string,age:number,price:number){
-    this.id=crypto.randomUUID();
-    this.name = name;
-    this.species = species;
-    this.age = age;
-    this.price = price;
-  }
-}
+app.use('/pets',petsRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
