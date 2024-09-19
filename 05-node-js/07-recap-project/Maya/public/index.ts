7a23894d79a55dba4d782a45022d499f33a7a9d0
Mayalvy <mayalevy20@gmail.com>
2024-09-19 19:31:08 +0300

    route

--- 05-node-js/07-recap-project/Maya/public/index.ts
+++ 05-node-js/07-recap-project/Maya/public/index.ts
@@ -1,1 +1,18 @@
 console.log('Hello, TypeScript!');
+
+class Pet{
+    Id: String;
+    species: String
+    yearOfBirth: number;
+    price: number;
+    imageUrl: string;
+    constructor( species:String, age:number,price:number,imageUrl:string){
+    this.Id = Math.random().toString();
+      this.species = species;
+      this.yearOfBirth = new Date().getFullYear() - age;
+      imageUrl = imageUrl;
+      this.price = price;
+    }
+    }
+
+    const pets:Pet[] = []
