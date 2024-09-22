bcb1fb0b0f11572dbe15d2e5bfa9eb1522d575ab
ShorY <yosishor28@gmail.com>
2024-09-22 17:56:50 +0300

    add delete task route

--- 05-node-js/07-recap-project/Yos/src/routes/tasks.ts
+++ 05-node-js/07-recap-project/Yos/src/routes/tasks.ts
@@ -1,50 +1,65 @@
-const tasksRouter = require('express').Router();
+import { randomInt } from "crypto";
 
+const tasksRouter = require("express").Router();
 
 class Task {
-    name: string;
-    id: string;
-    description: string;
-    constructor(name: string, description: string, id?: string) {
-        this.name = name;
-        this.description = description
-        this.id = id ?? crypto.randomUUID();
-    }
+  name: string;
+  id: string;
+  description: string;
+  constructor(name: string, description: string, id?: string) {
+    this.name = name;
+    this.description = description;
+    this.id = id ?? randomInt(1000).toString();
+  }
 }
 
 const tasks: Task[] = [
-    new Task('Task 1', 'This is the first task', '1'),
-    new Task('Task 2', 'This is the second task', '2'),
-    new Task('Task 3', 'This is the third task', '3'),
-    new Task('Task 4', 'This is the fourth task', '4'),
-]
+  new Task("Task 1", "This is the first task", "1"),
+  new Task("Task 2", "This is the second task", "2"),
+  new Task("Task 3", "This is the third task", "3"),
+  new Task("Task 4", "This is the fourth task", "4"),
+];
 
-tasksRouter.get('/all-tasks', (req: any, res: any) => {
-    res.json(tasks);
+tasksRouter.get("/all-tasks", (req: any, res: any) => {
+  res.json(tasks);
+});
+
+tasksRouter.get("/task/:id", (res: any, req: any) => {
+  const { id } = req.params;
+  console.log(id);
+  const task = tasks.find((task) => task.id === id);
+  if (task) {
+    res.json(task);
+  } else {
+    res.send({ error: "Task not found" });
+  }
+});
+
+tasksRouter.post("/add-task", (req: any, res: any) => {
+  const { name, description } = req.body;
+  if (!name || !description) {
+    res.status(400).send({ error: "Name and description are required" });
+  }
+  const task = new Task(name, description);
+  tasks.push(task);
+  res.send({ done: "Task added successfully", tasks: tasks });
 });
 
 
-tasksRouter.get('/task/:id', (res: any, req: any) => {
-    const { id } = req.params;
-    console.log(id)
-    const task = tasks.find(task => task.id === id)
-    if (task) {
-        res.json(task);
-    } else {
-        res.send({'error':'Task not found'});
-    }
 
-});
+tasksRouter.delete("/delete-task/", (req: any, res: any) => {
+  console.log(req.body);
+  const { id } = req.body;
+  console.log(req.body, id);
 
-
-tasksRouter.post('/add-task', (req:any, res:any) => {
-    const {name, description} = req.body;
-    if(!name || !description){
-        res.status(400).send({'error':'Name and description are required'});
-    }
-    const task = new Task(name, description);
-    tasks.push(task);
-    res.send({done:'Task added successfully', tasks: tasks});
+  const taskIndex = tasks.findIndex((task) => task.id === id);
+  if (taskIndex !== -1) {
+    tasks.splice(taskIndex, 1);
+    res.send({ done: "Task deleted successfully", tasks: tasks });
+    return;
+  }
+  res.send({ error: "Task not found" });
+  return; 
 });
 
 module.exports = tasksRouter;
