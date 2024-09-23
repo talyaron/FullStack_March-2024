fec20127f37c5febbe643ae01cd27ee3ebe9861a
orlyjonathan <168757564+orlyjonathan@users.noreply.github.com>
2024-09-23 15:46:36 +0300

    changes

--- 05-node-js/07-recap-project/Orly/src/routes/petsRoutes.ts
+++ 05-node-js/07-recap-project/Orly/src/routes/petsRoutes.ts
@@ -1,9 +1,15 @@
 import express from 'express'
 import {get} from 'http';
+import {  getPetByID, updatePet, deletePet } from '../controller/getPetsCont';
 import{getAllPets} from '../controller/getPetsCont';
+import { addPet } from '../controller/setPetsCont';
 const router = express.Router()
 
-router.get('/get-all-pets', getAllPets)
+router.get('/get-all-pets', getAllPets);
+router.post('/setPets', addPet);  
+router.get('/pets/:id', getPetByID);    
+router.put('/pets/:id', updatePet);      
+router.delete('/pets/:id', deletePet);
 
-export default router
+export default router;
 
