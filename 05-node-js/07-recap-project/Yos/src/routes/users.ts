64e835bf790b122c372ab3e4aedcfb39a6b019b3
ShorY <yosishor28@gmail.com>
2024-09-23 18:22:45 +0300

    handle add users

--- 05-node-js/07-recap-project/Yos/src/routes/users.ts
+++ 05-node-js/07-recap-project/Yos/src/routes/users.ts
@@ -1,30 +1,43 @@
-const route = require('express');
+import { randomInt } from "crypto";
+
+const route = require("express");
 const router = route.Router();
 
-class User{
-    name:string;
-    id:string;
-    age:number;
-    title:string;
-    constructor(name:string,  age:number, title:string, id:string,){
-        this.name = name;
-        this.id = crypto.randomUUID();
-        this.age = age;
-        this.title = title;
-    }
+class User {
+  name: string;
+  id: number;
+  age: number;
+  title: string;
+  constructor(name: string, age: number, title: string, id?: number) {
+    this.name = name;
+    this.id = id ?? randomInt(1, 1000);
+    this.age = age;
+    this.title = title;
+  }
 }
 
-const users:User[] = [
-    new User('John', 23, 'Mr', '1'),
-    new User('Jane', 24, 'Mrs', '2'),
-    new User('Dan', 25, 'Dr', '1234'),
-    new User('Doe', 26, 'Prof', '12345'),
-]
+const users: User[] = [
+  new User("John", 23, "Mr", 1),
+  new User("Jane", 24, "Mrs", 2),
+  new User("Dan", 25, "Dr", 1234),
+  new User("Doe", 26, "Prof", 12345),
+];
 
+router.get("/all-users", (req: any, res: any) => {
+  res.json(users);
+});
 
-router.get('/all-users', (req:any, res:any) => {
-    res.json(users);
-})
+router.post("/add-user", (req: any, res: any) => {
+  try {
+    const { name, age, title } = req.query;
+    console.log(name, age, title);
+    const newUser = new User(name, age, title, randomInt(1, 1000));
+    users.push(newUser);
+    res.send({ users: users });
+  } catch (error) {
+    res.status(400).json({ message: error });
+    return;
+  }
+});
 
-
-module.exports = router
+module.exports = router;
