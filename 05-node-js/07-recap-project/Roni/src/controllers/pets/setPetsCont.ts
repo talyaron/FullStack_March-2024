3b0df0984c0d5d8c09a6c8b0bdf8c0a445fdba39
Roni-Hoshen <ronihoshen100@gmail.com>
2024-09-23 15:35:31 +0300

    update

---
+++ 05-node-js/07-recap-project/Roni/src/controllers/pets/setPetsCont.ts
@@ -0,0 +1,17 @@
+import {Pet, pets} from "../../model/petsModel";
+
+export function addPet(req:any, res:any){
+    try {
+        const { name, species, age, price} = req.body;
+        if(!name || !species || !age || !price){
+            return res.status(400).send('Missing Data');
+        }
+        const newPet = new Pet(name, species, age, price);
+        pets.push(newPet);
+        res.status(200).send({newPet, ok:true})
+        
+    } catch (error:any) {
+        console.error(error);
+        res.status(500).send('Internal server error',error.message);
+    }
+}
