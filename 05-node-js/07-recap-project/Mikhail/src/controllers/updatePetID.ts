6537e0b9a6243d30302740fc354b05e0812863be
HotNuggets <mishamandark@gmail.com>
2024-09-26 10:40:58 +0300

    update

--- 05-node-js/07-recap-project/Mikhail/src/controllers/updatePetID.ts
+++ 05-node-js/07-recap-project/Mikhail/src/controllers/updatePetID.ts
@@ -1,26 +1,7 @@
 import { pets } from '../../src/model/petModel'
 
-
-export function updatePetID(req:any,res:any){
-    try {
-    
-        const {petID} = req.params;
-        const {price,species,name,age} = req.body;
-        console.log(petID);
-            
-        
-        if (!petID ) throw new Error("bad pet ID");
-        
-            
-            
-        const petsIndex = pets.findIndex(pet => pet.id === petID);
-        if (petsIndex === -1) throw new Error("PET not found");
-    
-        if(age)pets[petsIndex].setAge(age);
-        if(price)pets[petsIndex].setPrice(price);
-        if(species)pets[petsIndex].setSpecies(species);
-        if(name) pets[petsIndex].setName(name);
-        
+export function updatePetID(req: any, res: any) {
+  try {
 
     const { petID } = req.params;
     const { price, age,species,name } = req.body;
