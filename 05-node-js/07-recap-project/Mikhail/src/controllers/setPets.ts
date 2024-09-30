3031d67ed0b333a6a32e6bef5a65b3bdadd1dc03
HotNuggets <mishamandark@gmail.com>
2024-09-30 09:27:32 +0300

    update

--- 05-node-js/07-recap-project/Mikhail/src/controllers/setPets.ts
+++ 05-node-js/07-recap-project/Mikhail/src/controllers/setPets.ts
@@ -2,12 +2,16 @@ import { Pet ,pets} from "../model/petModel";
 export function addPet(req:any, res:any){
     try {
         
-        const {name,species,age,price}=req.body;
-        if(!name || !species || !age || ! price) {
+        const {name,species,age,price,imageURL}=req.body;
+        console.log(req.body)
+      
+        if(!name || !species || !age || ! price || !imageURL) {
+            console.log("im here")
             return res.status(400).send("missing data")
         }
-        const newPet=new Pet(name,species,age,price);
+        const newPet=new Pet(name,species,age,price,imageURL);
         pets.push(newPet);
+        console.log(newPet)
         res.send({newPet,ok:true});
     } catch (error:any) {
         console.error(error);
