23662bd8489dac3ce0255d7508c9ff76cce220b2
HotNuggets <mishamandark@gmail.com>
2024-10-04 15:12:51 +0300

    update

---
+++ 06-MongoDB/04-Project-no-cookies-30-09-2024/mikhail/src/server.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+
+const app = express()
+const port = 3000
+
+app.use(express.json());
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://HotNuggets:ieoBkUDlsayOqaoT@cluster0.z25jn.mongodb.net/').then(()=>{
+  console.log('connected to db')
+})
+.catch((err:any)=>{
+  console.log(err)
+});
+
+app.use(express.static('public'));
+
+
+
+app.get('/', (req:any, res:any) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
