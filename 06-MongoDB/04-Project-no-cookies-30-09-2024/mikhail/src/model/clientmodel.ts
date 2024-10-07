8abb1613c8a8fa20936dafd8610a30615090f692
HotNuggets <mishamandark@gmail.com>
2024-10-07 19:09:33 +0300

    update

--- 06-MongoDB/04-Project-no-cookies-30-09-2024/mikhail/src/model/clientmodel.ts
+++ 06-MongoDB/04-Project-no-cookies-30-09-2024/mikhail/src/model/clientmodel.ts
@@ -3,6 +3,9 @@ import mongoose from "mongoose";
 const clientSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    dateOfBirth: { type: Date, required: true },
+    
     
   });
 
