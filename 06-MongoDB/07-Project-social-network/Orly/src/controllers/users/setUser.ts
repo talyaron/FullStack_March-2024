e050ccef2b4a8f092d7fef18a45c024f79bad83e
orlyjonathan <168757564+orlyjonathan@users.noreply.github.com>
2024-10-14 20:22:06 +0300

    changes

---
+++ 06-MongoDB/07-Project-social-network/Orly/src/controllers/users/setUser.ts
@@ -0,0 +1,24 @@
+import{Request, Response} from 'express';
+
+export function login(req: Request, res: Response) {
+    try {
+        const{ username, password} = req.body;
+
+        res.status(200).send({username, password});
+        
+    } catch (error) {
+        console.error(error)
+        res.status(500).send(error);
+    }
+}
+
+export function register(req: Request, res:Response){
+    try {
+        const{username, password} =req.body;
+
+        res.status(200).send({username, password})
+    } catch (error) {
+        console.error(error)
+        res.status(500).send (error)
+    }
+}
---
+++ 06-MongoDB/07-Project-social-network/Orly/src/routes/userRoutes.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import { login, register } from "../controllers/users/setUser";
+
+const router = express.Router();
+
+router.post("/login", login).post("/register", register);
+
+
+export default router;
