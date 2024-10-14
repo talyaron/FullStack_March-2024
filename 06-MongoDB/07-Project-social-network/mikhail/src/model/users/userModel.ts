8030494ce011559b7a4298bb95865859b4b20784
HotNuggets <mishamandark@gmail.com>
2024-10-14 19:18:30 +0300

    project

---
+++ 06-MongoDB/07-Project-social-network/mikhail/src/model/users/userModel.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export const UserSchema = new mongoose.Schema({
+    name:String,
+    email:{
+        type:String,
+        unique:true
+    },
+    password:String
+})
---
+++ 06-MongoDB/07-Project-social-network/mikhail/src/server.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import usersRouter from './routes/users/usersRoutes';
+const app = express();
+const port = 3000;
+
+
+//connection to db
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://HotNuggets:ieoBkUDlsayOqaoT@cluster0.z25jn.mongodb.net/instagram').then(()=>{
+  console.log('connected to db')
+})
+.catch((err:any)=>{
+  console.log(err)
+});
+
+
+app.use(express.json());
+
+
+
+app.use(express.static('public'));
+
+app.use('/users',usersRouter)
+
+
+app.get('/', (req:any, res:any) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
