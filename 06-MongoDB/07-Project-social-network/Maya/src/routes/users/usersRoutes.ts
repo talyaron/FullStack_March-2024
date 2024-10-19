ef7704765dca382f93ad36c57f90e158e5a2a3aa
Mayalvy <mayalevy20@gmail.com>
2024-10-19 15:09:55 +0300

    update

--- 06-MongoDB/07-Project-social-network/Maya/src/routes/users/usersRoutes.ts
+++ 06-MongoDB/07-Project-social-network/Maya/src/routes/users/usersRoutes.ts
@@ -2,8 +2,13 @@ import express from "express";
 import { Request, Response } from 'express';
 import { User } from '../../model/userModel';
 import { getUser } from "../../controllers/users/getUser";
+import { login } from "../../controllers/users/dist/login";
+import { register } from "../../controllers/users/dist/
+";
+
+const router = express.Router();
+
 
 router.post("/login", login).post("/register", register).get("/getUser", getUser);
-
 
 export default router;
