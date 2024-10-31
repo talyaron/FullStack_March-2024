6282a7959b44eecc579dfbbe8bce031ea1cb47f5
israelcohen1 <134219173+israelcohen1@users.noreply.github.com>
2024-10-31 16:57:06 +0200

    ADD

---
+++ 06-MongoDB/08- Middleware/ClassPuzzle/israel/src/server.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import usersRouter from './routes/users/usersRoutes';
+import PetsRouter from './routes/pets/petsRoutes';
+const app = express();
+import cookieParser from 'cookie-parser';
+const port = 3000;
+
+
+//connection to db
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://israelcohen:lfOnkcYdjRmupQI9@cluster0.8dupj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=>{
+  console.log('connected to db')
+})
+.catch((err:any)=>{
+  console.log(err)
+});
+
+//middlewares
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.use(express.static('public'));
+
+app.use('/users',usersRouter);
+app.use('/pets',PetsRouter);
+
+
+app.get('/', (req:any, res:any) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
