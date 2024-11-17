ae766e05bf9d91dc53ab840e0b9a717b0114bb90
Olga Larina <150347557+21apr@users.noreply.github.com>
2024-11-17 10:04:25 +0000

    chess init

--- 07-React/02-Using-components/HomePuzzle/olga/src/view/components/Tile/Tile.tsx
+++ 07-React/02-Using-components/HomePuzzle/olga/src/view/components/Tile/Tile.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
-import './Tile.scss';
+import { FC } from "react";
+import "./Tile.scss";
+import Piece from "../pieses/Piece";
 
 interface TileProps {
   black: boolean;
-};
+  piece?: {
+    type: "pawn" | "rook" | "knight" | "bishop" | "queen" | "king";
+    color: "white" | "black";
+  };
+  selected: boolean;
+  onClick: () => void;
+}
 
-const Tile: React.FC<TileProps> = ({ black }) => {
-  const tileClass = black ? 'tile black' : 'tile white';
-  return <div className={tileClass}></div>;
+const Tile: FC<TileProps> = ({ black, selected, onClick, piece }) => {
+  const tileClass = `tile ${black ? "black" : "white"} ${
+    selected ? "selected" : ""
+  }`;
+
+  if (!piece) {
+    return <div className={tileClass} onClick={onClick} />;
+  } else {
+    return (
+      <div className={tileClass} onClick={onClick}>
+        {piece && <Piece type={piece.type} color={piece.color} />}
+      </div>
+      );
+  }
+  
 };
 
 export default Tile;
