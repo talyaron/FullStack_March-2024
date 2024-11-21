b9a66f7152a0762bf77bfed769424e8cbdaea35f
crocodama <86365509+crocodama@users.noreply.github.com>
2024-11-21 19:12:29 +0200

    aa

--- 07-React/04-react-router/followup/Naama/src/page2/Page2.tsx
+++ 07-React/04-react-router/followup/Naama/src/page2/Page2.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Page2 = () => {
   return (
-    <div className='page'>page2</div>
+    <div className='page'>page2
+    <Link to="/">to home page</Link>
+    <Link to="/page1">to page1</Link>
+    </div>
   )
 }
 
