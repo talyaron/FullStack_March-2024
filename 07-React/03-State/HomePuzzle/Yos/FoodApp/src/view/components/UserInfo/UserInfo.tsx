8cf4938b149e9ff656933bc9772b8d490684ca9e
ShorY <yosishor28@gmail.com>
2024-11-21 13:47:18 +0200

    build cat images api with food homepuzzle

--- 07-React/03-State/HomePuzzle/Yos/FoodApp/src/view/components/UserInfo/UserInfo.tsx
+++ 07-React/03-State/HomePuzzle/Yos/FoodApp/src/view/components/UserInfo/UserInfo.tsx
@@ -6,10 +6,12 @@ interface UserInfoProps {
         avatar_url: string,
         location: string,
         name: string,
-        created_at: string,
         followers: number,
         following: number,
         public_repos: number,
+        created_at: string,
+        updated_at: string
+
     }
 }
 
@@ -18,6 +20,7 @@ const UserInfo: FC<UserInfoProps> = ({ userData }) => {
 
     return (
         <>
+            
             <div className='user-wrapper'>
                 <div className="user-image">
                     <img src={userData.avatar_url} alt="avatar" />
@@ -28,6 +31,8 @@ const UserInfo: FC<UserInfoProps> = ({ userData }) => {
                     <div className="user-followers">Followers: {userData.followers}</div>
                     <div className="user-following">Following: {userData.following}</div>
                     <div className="user-repos">Public Repos: {userData.public_repos}</div>
+                    <div className="user-created">Created At: {userData.created_at.split('T')[0]}</div>
+                    <div className="user-updated">Updated At: {userData.updated_at.split('T')[0]}</div>
                 </div>
             </div>
         </>
