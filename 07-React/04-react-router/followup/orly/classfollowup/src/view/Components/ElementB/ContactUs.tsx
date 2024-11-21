1e4f0e9b5148ae5dd8abd38393d4b19d04bbc74a
orlyjonathan <168757564+orlyjonathan@users.noreply.github.com>
2024-11-21 19:49:49 +0200

    chnages

---
+++ 07-React/04-react-router/followup/orly/classfollowup/src/view/Components/ElementB/ContactUs.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ContactUs = () => {
+    return (
+        <div>
+            <h1>Contact Us</h1>
+            <Link to="/">Home</Link>
+        </div>
+    );
+};
+
+export default ContactUs;
--- 07-React/04-react-router/followup/orly/classfollowup/src/view/page2/Page2.tsx
+++ 07-React/04-react-router/followup/orly/classfollowup/src/view/page2/Page2.tsx
@@ -1,13 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const Page2 = () => {
     return (
         <div className="page">
             <h1>Page 2</h1>
             <Link to="/">Go to Page1</Link>
+            <nav>
+                <Link to="About">About</Link>
+                <Link to="ContactUs">ContactUs</Link>
+                <Link to="Home">Home</Link>
+            </nav>
+            <div className="element">
+                <Outlet />
         </div>
-    )
-}
+        </div>
+    );
+};
 
 export default Page2;
