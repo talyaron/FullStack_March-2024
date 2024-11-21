aeda23b86396b01ba7c041e7a8af48c16cfdea42
orlyjonathan <168757564+orlyjonathan@users.noreply.github.com>
2024-11-21 18:28:32 +0200

    changes

--- 07-React/04-react-router/followup/orly/classfollowup/src/router.tsx
+++ 07-React/04-react-router/followup/orly/classfollowup/src/router.tsx
@@ -4,7 +4,12 @@ import Page2 from './view/page2/Page2';
 
 export const router = createBrowserRouter([
     {
-        path: '/',
-        element: <div>Hello World</div>,
+      path: "/",
+      element: <Page1 />,
     },
-])
+    {
+      path: "/page2",
+      element:<Page2 />,
+    }
+  ]);
+  
--- 07-React/04-react-router/followup/orly/classfollowup/src/view/page1/Page1.tsx
+++ 07-React/04-react-router/followup/orly/classfollowup/src/view/page1/Page1.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Page1 = () => {
     return (
-        <div className="page">
+        <div className="page" style={{backgroundColor:"pink"}}>
             <h1>Page 1</h1>
+            <Link to="/page2">Go to Page 2</Link>
         </div>
     )
 }
 
-export default Page1
+export default Page1;
