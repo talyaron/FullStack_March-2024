babc679acd86d914c9166e37e5feb6a1f7586429
ShorY <yosishor28@gmail.com>
2024-11-25 15:37:20 +0200

    add assignment

--- 07-React/04-react-router/followup/yoss/ReactRouterExample/src/main.tsx
+++ 07-React/04-react-router/followup/yoss/ReactRouterExample/src/main.tsx
@@ -6,6 +6,8 @@ import { RouterProvider } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    
     <RouterProvider router={router} />
+
     </StrictMode>,
 )
