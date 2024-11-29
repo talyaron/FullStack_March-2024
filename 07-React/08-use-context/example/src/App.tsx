a12210b07096e79668718418b1477b1b58f1a2f1
Tal Yaron <tal.yaron@gmail.com>
2024-11-29 08:17:38 +0200

    cont

--- 07-React/08-use-context/example/src/App.tsx
+++ 07-React/08-use-context/example/src/App.tsx
@@ -1,16 +1,28 @@
-const ThemeContext = createContext();
+import { useState } from "react";
+import { ThemeContext } from "./AppCont";
+import Header from "./view/header/Header";
+import './App.css';
 
-// Theme provider component
-const ThemeProvider = ({ children }) => {
-  const [isDark, setIsDark] = useState(false);
+const App = () => {
+  const [theme, setTheme] = useState("light");
 
-  const toggleTheme = () => {
-    setIsDark(prev => !prev);
-  };
+  function handleToggleTheme() {
+    setTheme(theme === "light" ? "dark" : "light");
+  }
 
   return (
-    <ThemeContext.Provider value={{ isDark, toggleTheme }}>
-      {children}
+    <ThemeContext.Provider
+      value={{
+        mode: theme,
+        toggleTheme: handleToggleTheme,
+      }}
+    >
+      <div className={theme === "dark"?"dark app":"light app"}>
+        <Header />
+        <h1>App</h1>
+        <button onClick={handleToggleTheme}>Toggle</button>
+      </div>
     </ThemeContext.Provider>
   );
 };
+export default App;
--- 07-React/08-use-context/example/src/view/header/Header.tsx
+++ 07-React/08-use-context/example/src/view/header/Header.tsx
@@ -1,14 +1,14 @@
-import exp from "constants";
+import { useContext } from "react";
+import { ThemeContext } from "../../AppCont";
 
 const Header = () => {
-  const { isDark, toggleTheme } = useContext(ThemeContext);
+ 
+  const {mode,toggleTheme} = useContext(ThemeContext);
 
   return (
-    <div className="flex justify-between items-center p-4 mb-4">
-      <h1 className="text-xl font-bold">Theme Example</h1>
-      <Button onClick={toggleTheme} variant="outline" size="icon">
-        {isDark ? <Sun className="h-4 w-4" /> : <Moon className="h-4 w-4" />}
-      </Button>
+    <div >
+     theme: {mode}
+<button onClick={toggleTheme}>Toggle</button>
     </div>
   );
 };
