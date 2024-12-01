77a36a710ba4ba2e3aec52ca3becc3a43aae0f84
ShorY <yosishor28@gmail.com>
2024-12-01 09:41:49 +0200

    get highest number from inputs

--- 07-React/07-lifting-state-up/classPuzzle/YosMikh/prject/src/App.tsx
+++ 07-React/07-lifting-state-up/classPuzzle/YosMikh/prject/src/App.tsx
@@ -1,37 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import './App.scss'
 import Input from './views/Components/inputs/Input'
 
 function App() {
-  const [number, setNumber] = useState(0)
-  console.log(number)
+  const [number1, setNumber1] = useState(0)
+  const [number2, setNumber2] = useState(0)
+  const [number3, setNumber3] = useState(0)
+  const [number4, setNumber4] = useState(0)
+  const [higherNumber, setHigherNumber] = useState(0)
 
-  const [list, setList] = useState<number[]>([])
 
 
-  function handleSubmit(event: any) {
-    event.preventDefault();
-    const form = event.target.input;
-    form.forEach((element : any) => {
-      const { valueAsNumber } = element;
-      list.push(valueAsNumber)
-      // setList([...list, valueAsNumber])
-      console.log(list)
-    });
-  }
+  useEffect(() => {
+    setHigherNumber(Math.max(number1, number2, number3, number4));
+  }, [number1, number2, number3, number4])
+
 
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <Input setNumber={setNumber} />
-        <Input setNumber={setNumber} />
-        <Input setNumber={setNumber} />
-        <Input setNumber={setNumber} />
-        <button type="submit" >pop</button>
-      </form>
-    </>
+    <div className="app-container">
+    <h1>Find the Highest Number</h1>
+    <div className="inputs-container">
+      <Input setNumber={setNumber1} />
+      <Input setNumber={setNumber2} />
+      <Input setNumber={setNumber3} />
+      <Input setNumber={setNumber4} />
+    </div>
+    <div className="results">
+      <p>Total: {number1 + number2 + number3 + number4}</p>
+      <p>Highest Number: {higherNumber}</p>
+    </div>
+  </div>
   )
 }
 
