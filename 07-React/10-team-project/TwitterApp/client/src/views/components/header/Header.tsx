79b1ed233e3497f82f4526c68d34b46df465c2cd
ShorY <yosishor28@gmail.com>
2024-12-04 18:19:52 +0200

    add some test react components

--- 07-React/10-team-project/TwitterApp/client/src/views/components/header/Header.tsx
+++ 07-React/10-team-project/TwitterApp/client/src/views/components/header/Header.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import './Header.scss'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+      {/* Header Section */}
+      <header className='header'>
+        <h1 className='title'>Twitter</h1>
+        <div className='userActions'>
+          <a href="./login/index.html">
+            <button className='button'>Sign In</button>
+          </a>
+          <a href="./register/index.html">
+            <button className='button'>Register</button>
+          </a>
+        </div>
+      </header>
+    </div>
   )
 }
 
