36b012941d80c775466bf35ceb9121ad964cb9b9
Tal Yaron <tal.yaron@gmail.com>
2024-12-26 19:46:48 +0200

    wip

--- 07-React/14-overview-26-12-2024/classPuzzle/client/src/view/pages/main/Main.tsx
+++ 07-React/14-overview-26-12-2024/classPuzzle/client/src/view/pages/main/Main.tsx
@@ -6,6 +6,20 @@ const Main = () => {
         <Link to="/login">Login</Link>
         <br />
         <Link to="/register">Register</Link>
+
+          <form className="form">
+              <div className="form__group">
+                  <label className="form__label">Name</label>
+                  <input type="text" placeholder="Enter name" />
+              </div>
+
+              <div className="form__group">
+                  <label className="form__label">Category</label>
+                  <select>
+                      <option>Select category</option>
+                  </select>
+              </div>
+          </form>
     </div>
   )
 }
