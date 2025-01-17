24947549c188886784f400a64b16c0b97c847eb8
Tal Yaron <tal.yaron@gmail.com>
2025-01-17 15:37:22 +0200

    cont

--- 07-React/16-DragNDrop-16-1-2025/example/src/App.tsx
+++ 07-React/16-DragNDrop-16-1-2025/example/src/App.tsx
@@ -5,15 +5,34 @@ import styles from "./App.module.scss";
 function App() {
 
   const [position, setPosition] = useState({ x: 0, y: 0 })
-
+  const [grabPointDelta, setGrabPointDelta] = useState({ deltaX: 0, deltaY: 0 })
 
   function handleDragEnd(event: React.DragEvent<HTMLDivElement>) {
-    console.log(event)
-    setPosition({ x: event.clientX, y: event.clientY });
+    console.log("new Position: ", event.clientX - grabPointDelta.deltaX, event.clientY - grabPointDelta.deltaY)
+    setPosition({ x: event.clientX, y: event.clientY});
+  }
+  function handleDragStart(event: React.DragEvent<HTMLDivElement>) {
+    console.log("Previous: ", position.x, position.y)
+    console.log("dragPoint: ", event.clientX, event.clientY)
+    setGrabPointDelta({
+      deltaX: event.clientX - position.x,
+      deltaY: event.clientY - position.y,
+    });
   }
 
-  function handleDrag(event: React.DragEvent<HTMLDivElement>) {
-    console.log(event)
+  function handleDragOver(event: React.DragEvent<HTMLDivElement>) {
+    event.preventDefault();
+    console.log("dragOver")
+  }
+
+  function handleDragLeave(event: React.DragEvent<HTMLDivElement>) {
+    event.preventDefault();
+    console.log("drag leave")
+  }
+
+  function handleDrop(event: React.DragEvent<HTMLDivElement>) {
+    event.preventDefault();
+    setPosition({ x: event.clientX - grabPointDelta.deltaX, y: event.clientY - grabPointDelta.deltaY });
   }
 
   return (
@@ -22,9 +41,24 @@ function App() {
         className={styles.box}
         draggable
         onDragEnd={handleDragEnd}
-        onDrag={handleDrag}
+
+        onDragStart={handleDragStart}
         style={{ left: position.x, top: position.y }}
       />
+
+      <div>
+        <h2>Drag me around</h2>
+        <p>Position: {position.x}, {position.y}</p>
+      </div>
+      <div 
+      className={styles.dropZone}
+        onDragOver={handleDragOver}
+        onDragLeave={handleDragLeave}
+        onDrop={handleDrop}
+      >
+        Drop Zone
+
+      </div>
 
     </>
   )
