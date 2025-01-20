8ab8a2ee194ee366f8d3346f3e05acae9be85c22
ShorY <yosishor28@gmail.com>
2025-01-20 17:56:34 +0200

    drag and drop exmaple

--- 07-React/16-DragNDrop-16-1-2025/example/src/App.tsx
+++ 07-React/16-DragNDrop-16-1-2025/example/src/App.tsx
@@ -5,15 +5,29 @@ import styles from "./App.module.scss";
 function App() {
 
   const [position, setPosition] = useState({ x: 0, y: 0 })
-
+  const [delta, setDelta] = useState({ x: 0, y: 0 })
 
   function handleDragEnd(event: React.DragEvent<HTMLDivElement>) {
-    console.log(event)
+    console.log("end", event.clientX, event.clientY)
+    console.log("delta", delta.x, delta.y)
+    // setDelta({ x: event.clientX - position.x, y: event.clientY - position.y })
     setPosition({ x: event.clientX, y: event.clientY });
+    // console.log(event.clientX, event.clientY, event.screenX, event.screenY, event.pageX, event.pageY)
   }
+  const handleDragStart = (event: React.DragEvent<HTMLDivElement>) => {
+    setDelta({ x: event.clientX - position.x, y: event.clientY - position.y })
+    console.log("delta", delta.x, delta.y)
 
+    console.log("previous", position.x, position.y)
+    console.log("delta", delta.x, delta.y)
+    console.log("end", event.clientX, event.clientY)
+    console.log("current", event.clientX + position.x, event.clientY + position.y)
+
+    // console.log(event.clientX, event.clientY, event.screenX, event.screenY, event.pageX, event.pageY)
+  }
   function handleDrag(event: React.DragEvent<HTMLDivElement>) {
-    console.log(event)
+    // console.log(event)
+
   }
 
   return (
@@ -24,6 +38,7 @@ function App() {
         onDragEnd={handleDragEnd}
         onDrag={handleDrag}
         style={{ left: position.x, top: position.y }}
+        onDragStart={handleDragStart}
       />
 
     </>
