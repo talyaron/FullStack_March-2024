c375bc438c39d9910f9cc87433f5de8a42f2ca4c
olga.larin <21apr1988@gmail.com>
2025-01-27 19:42:16 +0200

    classPuzzle

---
+++ 07-React/19-Redux-toolkit-async-27-01-2025/classPuzzle1/olga/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import UsersList from './view/components/user/UserList';
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <UsersList />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ 07-React/19-Redux-toolkit-async-27-01-2025/classPuzzle1/olga/src/model/store.ts
@@ -0,0 +1,20 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { userApi } from '../services/user';
+
+
+export const store = configureStore({
+    reducer: {
+        [userApi.reducerPath]: userApi.reducer,
+    },
+
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(userApi.middleware),
+
+})
+
+setupListeners(store.dispatch)
+
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
