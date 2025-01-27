bcd97582c38bb39bd8c56fee51a63b027d0f6c12
MayaLY <maya_levy@yahoo.com>
2025-01-27 19:15:52 +0200

    class puzzle not done

---
+++ 07-React/19-Redux-toolkit-async-27-01-2025/classPuzzle1/Maya/src/main.tsx
@@ -0,0 +1,21 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { Provider } from 'react-redux'
+import { store } from './model/store.ts'
+import { BrowserRouter, Routes, Route } from "react-router";
+import Nationality from './view/components/nationality/Nationality.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/nationality/:name" element={<Nationality />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>,
+  </StrictMode>,
+)
---
+++ 07-React/19-Redux-toolkit-async-27-01-2025/classPuzzle1/Maya/src/services/nationality.ts
@@ -0,0 +1,15 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { Nationality } from "../model/models/nationalityModel";
+
+// Define a service using a base URL and expected endpoints
+export const nationalApi = createApi({
+    reducerPath: 'nationalApi',
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://api.nationalize.io/' }),
+    endpoints: (builder) => ({
+        getNationalityByName: builder.query<Nationality, string>({
+            query: (name) => `?name=${name}`, // Correct query string format
+        }),
+    }),
+});
+
+export const { useGetNationalityByNameQuery } = nationalApi; // Fixed naming
