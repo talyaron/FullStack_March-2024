039ded680c0fac48c69a446ed2192708ec12d918
ShorY <yosishor28@gmail.com>
2025-03-31 12:29:01 +0300

    test android app

--- 13-ReactNative/02-base-components/example/App.tsx
+++ 13-ReactNative/02-base-components/example/App.tsx
@@ -3,13 +3,19 @@ import { useState } from 'react';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('');
 
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Open up App.tsx to start 333 working on your app! dhfksjdhklj sdh gkljsdh gkljshgd kljsh lkjhg lkdfh glkjdf hglkfdjg</Text>
+
+      <Text style={styles.text}>{text}</Text>
       <StatusBar style="auto" />
-      <TextInput style={styles.input} />
+      <TextInput style={styles.input} 
+        placeholder="Enter your text"
+        onChangeText={setText}
+        value={text}
+      />
     </View>
   );
 }
