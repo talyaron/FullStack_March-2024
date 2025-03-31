ad27d3722fc9c3185cb64ba56374662cab54445f
Tal Yaron <tal.yaron@gmail.com>
2025-03-31 18:30:36 +0300

    static ready

--- 13-ReactNative/04-navigation/followup/tal/App.tsx
+++ 13-ReactNative/04-navigation/followup/tal/App.tsx
@@ -2,10 +2,19 @@ import * as React from 'react';
 import Home from './pages/home/Home';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createStaticNavigation } from '@react-navigation/native';
+import Details from './pages/details/Details';
 
 const RootStack = createNativeStackNavigator({
+  initialRouteName: 'Home',
   screens: {
-    Home: Home,
+    Home:{
+      screen: Home,
+      name: 'Home',
+      options: {
+        title: 'Overview',
+      },
+    },
+    Details: Details,
   },
 });
 
